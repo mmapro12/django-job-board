6ce43bc885e40e6c8e9319577687359f0d8de6e3
mmapro12 <asia172007@gmail.com>
2024-03-13 19:38:50 +0300

    Add Job Views

--- job/models.py
+++ job/models.py
@@ -17,6 +17,14 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.name
